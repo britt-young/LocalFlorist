9a18978445be9c1f0a429b06f7098fcd183122e7
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-01-26 16:43:55 -0500

    custom tailwind styling and nav refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#404045', // light purple
+        'secondary': '#9F9FAB', // dark purple
+        'tertiary': '#E1E0DA', // neutral cream
+        'light': '#F8F8F8', // white-ish
+        'dark': '#000000', // black
+      },
+    },
   },
   plugins: [],
 }

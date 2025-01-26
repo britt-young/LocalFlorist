fccc8f09cf7736c9305ab1b2ba21da38d0dd64a8
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-01-26 16:51:43 -0500

    custom page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import RootLayout from './layout/RootLayout'
-import Page1 from './pages/Page1'
-import Page2 from './pages/Page2'
-import Page3 from './pages/Page3'
-import Page4 from './pages/Page4'
-import Page5 from './pages/Page5'
-import Error from './Components/Error'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Shop from './pages/Shop'
+import FAQs from './pages/FAQs'
+import Contact from './pages/Contact'
+import Error from './pages/Error'
 
 function App() {
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<RootLayout />}>
-      <Route index element={<Page1 />} />
-      <Route path ='page2' element={<Page2 />} />
-      <Route path ='page3' element={<Page3 />} />
-      <Route path ='page4' element={<Page4 />} />
-      <Route path ='page5' element={<Page5 />} />
+      <Route index element={<Home/>} />
+      <Route path ='about' element={<About />} />
+      <Route path ='shop' element={<Shop />} />
+      <Route path ='faqs' element={<FAQs />} />
+      <Route path ='contact' element={<Contact />} />
       {/* Can add nested routes too */}
       <Route path ='*' element={<Error />} />
     </Route>

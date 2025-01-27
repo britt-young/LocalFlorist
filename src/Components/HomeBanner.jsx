fcbad5ecf196c5e9f4e400222ff0c580fa1824c7
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-01-26 20:08:09 -0500

    banner bg fixed

--- src/Components/HomeBanner.jsx
+++ src/Components/HomeBanner.jsx
@@ -15,37 +15,9 @@ const HomeBanner = () => {
   if (!isVisible) return null; // If not visible, return null to remove the component
 
   return (
-    <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-100 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
-      {/* Decorative background 1 */}
-      <div
-        aria-hidden="true"
-        className="absolute top-1/2 left-[max(-7rem,calc(50%-52rem))] -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-      >
-        <div
-          style={{
-            clipPath:
-              "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
-          }}
-          className="aspect-577/310 w-[36.0625rem] bg-gradient from-red-700 to-blue-700 opacity-80"
-        />
-      </div>
-
-      {/* Decorative background 2 */}
-      <div
-        aria-hidden="true"
-        className="absolute top-1/2 left-[max(45rem,calc(50%+8rem))] -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-      >
-        <div
-          style={{
-            clipPath:
-              "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
-          }}
-          className="aspect-577/310 w-[36.0625rem] bg-gradient from-red-700 to-blue-700 opacity-80"
-        />
-      </div>
-
+    <div className="relative isolate flex items-center gap-x-6 overflow-hidden px-6 py-2.5 sm:px-3.5 sm:before:flex-1 bg-gradient-to-r from-primary to-secondary">
       <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
-        <p className="text-sm/6 text-gray-900">
+        <p className="text-sm/6 text-tertiary">
           <strong className="font-semibold uppercase">
             15% off all valentine's day arrangements
           </strong>
@@ -60,7 +32,7 @@ const HomeBanner = () => {
         </p>
         <a
           href="/shop"
-          className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-xs hover:bg-gray-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+          className="flex-none rounded-full bg-primary px-3.5 py-1 text-sm font-semibold text-white shadow-xs hover:bg-gray-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
         >
           Shop now <span aria-hidden="true">&rarr;</span>
         </a>

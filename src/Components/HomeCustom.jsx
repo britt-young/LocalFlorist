f77a7a658c288b36f8106734ea7fc8fbd913a3d5
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-01-27 15:22:13 -0500

    text add

--- src/Components/HomeCustom.jsx
+++ src/Components/HomeCustom.jsx
@@ -9,7 +9,7 @@ const HomeCustom = () => {
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mx-5 md:mx-60">
       
       <div className="md:mr-20 flex flex-col justify-center items-start text-left">
-        <h2 className="text-black mb-5">The Local Florist</h2>
+        <h2 className="text-black mb-5">Craving some Creative Control?</h2>
         <p className="text-primary">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -17,10 +17,10 @@ const HomeCustom = () => {
           aliquip ex ea commodo consequat
         </p>
         <Link
-          to="/about"
+          to="/contact"
           className="py-2 my-5 inline-block border-2 border-primary bg-tertiary px-6 pb-[6px] pt-2 text-xs font-medium leading-normal text-primary hover:bg-primary hover:text-tertiary"
         >
-          Read More
+          Place an order
         </Link>
       </div>
       <div className="flex justify-center">

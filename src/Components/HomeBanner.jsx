8a5c1a6322a4eab0e437415d7d41bcf8be8c95c9
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-01-26 19:50:14 -0500

    homepage promo banner add- needs bg refactoring!

---
+++ src/Components/HomeBanner.jsx
@@ -0,0 +1,82 @@
+// BACKGROUND COLOR GRADIENT REFACTORING NEEDED!!
+
+import React, { useState } from "react";
+import { XMarkIcon } from "@heroicons/react/20/solid";
+
+const HomeBanner = () => {
+  // State to manage visibility of the banner
+  const [isVisible, setIsVisible] = useState(true);
+
+  // Function to handle the closing of the banner
+  const closeBanner = () => {
+    setIsVisible(false);
+  };
+
+  if (!isVisible) return null; // If not visible, return null to remove the component
+
+  return (
+    <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-100 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+      {/* Decorative background 1 */}
+      <div
+        aria-hidden="true"
+        className="absolute top-1/2 left-[max(-7rem,calc(50%-52rem))] -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+      >
+        <div
+          style={{
+            clipPath:
+              "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
+          }}
+          className="aspect-577/310 w-[36.0625rem] bg-gradient from-red-700 to-blue-700 opacity-80"
+        />
+      </div>
+
+      {/* Decorative background 2 */}
+      <div
+        aria-hidden="true"
+        className="absolute top-1/2 left-[max(45rem,calc(50%+8rem))] -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+      >
+        <div
+          style={{
+            clipPath:
+              "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
+          }}
+          className="aspect-577/310 w-[36.0625rem] bg-gradient from-red-700 to-blue-700 opacity-80"
+        />
+      </div>
+
+      <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
+        <p className="text-sm/6 text-gray-900">
+          <strong className="font-semibold uppercase">
+            15% off all valentine's day arrangements
+          </strong>
+          <svg
+            viewBox="0 0 2 2"
+            aria-hidden="true"
+            className="mx-2 inline size-0.5 fill-current"
+          >
+            <circle r={1} cx={1} cy={1} />
+          </svg>
+          Now until February 13th
+        </p>
+        <a
+          href="/shop"
+          className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-xs hover:bg-gray-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+        >
+          Shop now <span aria-hidden="true">&rarr;</span>
+        </a>
+      </div>
+      <div className="flex flex-1 justify-end">
+        <button
+          type="button"
+          onClick={closeBanner}
+          className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
+        >
+          <span className="sr-only">Dismiss</span>
+          <XMarkIcon aria-hidden="true" className="size-5 text-gray-900" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default HomeBanner;

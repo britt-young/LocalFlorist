82bc6da6fe4c9401a29a57772d5d70979e3abf0b
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-06-25 13:02:57 -0400

    product page responsive styling

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -14,8 +14,8 @@ const Product = () => {
 
   return (
     <div className="py-20 max-w-6xl mx-auto">
-      <div className="flex gap-6">
-        <div className="w-1/2">
+      <div className="flex flex-col lg:flex-row gap-6 items-center">
+        <div className="lg:w-1/2 mx-5 lg:mx-0">
           {product.images.edges.length ? (
             <img
               src={product.images.edges[0].node.url}
@@ -28,16 +28,17 @@ const Product = () => {
             </div>
           )}
         </div>
-        <div className="my-5 w-1/3">
+        <div className="my-5 lg:w-1/3 mx-5 lg:mx-0">
           <h2 className="text-black mb-4">{product.title}</h2>
           <h5 className="text-secondary font-semibold mb-3">Size Options:</h5>
-          <ul className="mb-6">
+          <ul className="mb-6 flex lg:flex-row flex-col gap-2">
             {product.variants.edges.map(({ node }) => (
-              <li key={node.id} className="mb-0">
-                {node.title} — ${Number(node.price.amount).toFixed(2)} USD
+              <li key={node.id} className="mb-2 border-1 border-secondary py-1 px-2 w-fit hover:bg-secondary hover:text-primary cursor-pointer">
+                 {node.title} {/*— ${Number(node.price.amount).toFixed(2)} USD */}
               </li>
             ))}
           </ul>
+          <div className="text-primary flex lg:flex-col flex-col-reverse">
           <button
             className="py-2 w-full border-2 border-primary bg-primary px-4 text-sm font-medium leading-normal text-tertiary hover:bg-tertiary hover:text-primary cursor-pointer mb-4"
             onClick={() => addToCart(variant.id, 1)}
@@ -45,10 +46,7 @@ const Product = () => {
             Add to Cart
           </button>
           <p className="text-primary mb-6">{product.description}</p>
-
-          
-
-          
+          </div>          
         </div>
       </div>
     </div>

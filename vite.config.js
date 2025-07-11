2ec4d7ea384a501bf573708aafe6a9fab2589447
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-07-11 11:37:33 -0400

    removed vite svg

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr' //allows import of svg files as React components
 
 // https://vite.dev/config/
+// removed from plugins array (tailwindcss(),)
 export default defineConfig({
   plugins: [tailwindcss(), react(), svgr()],
 })

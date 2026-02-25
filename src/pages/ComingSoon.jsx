4e58cef0c1b906fb2f6f7921813b0f1a3b10c230
Britt Young <140761880+britt-young@users.noreply.github.com>
2026-02-25 13:01:32 -0500

    coming soon page

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -1,5 +1,5 @@
-import hero from "../assets/imgs/soon-hero.jpg"
-
+import hero from "../assets/imgs/soon-hero.jpg";
+import { Link } from "react-router-dom";
 const ComingSoon = () => {
   return (
     <div
@@ -7,15 +7,17 @@ const ComingSoon = () => {
       style={{ backgroundImage: `url(${hero})` }}
     >
       <div className="flex flex-col bg-transparent backdrop-blur-sm p-4 text-center">
-        <h1 className="text-center text-white">
-          Coming Soon
-        </h1>
-        <h3 className="text-center text-white mb-0">
-          our most commonly asked questions
-        </h3>
+        <h1 className="text-center text-white">Coming Soon</h1>
+        <h3 className="text-center text-white mb-0">please be patient</h3>
+        <Link
+          to="/shop"
+          className="py-2 inline-block border-2 border-primary bg-primary px-6 pb-[6px] pt-2 text-xs font-medium leading-normal text-tertiary hover:bg-tertiary hover:text-primary"
+        >
+          Browse our Bouquets
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ComingSoon
+export default ComingSoon;
